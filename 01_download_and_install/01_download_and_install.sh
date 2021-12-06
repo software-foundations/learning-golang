adcc4acb80f62785c1b3cf1882bda50c2065e6d1
brunoconde07 <brunoconde.ufpa@gmail.com>
2021-12-06 14:05:24 -0300

    install by snap

--- 01_download_and_install/01_download_and_install.sh
+++ 01_download_and_install/01_download_and_install.sh
@@ -8,5 +8,8 @@ rm -rf /usr/local/go
 sudo tar -C /usr/local -xzf go1.17.4.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 
+# -> Download (way 2)
+sudo snap install go --classic
+
 # -> Check
 go version
